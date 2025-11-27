06076895ed953bbb58b06421538e120a37e4db93
2061360308 <2061360308@qq.com>
2025-11-27 17:16:46 +0800

    fix:修复HeadLess

--- githubActionRun.py
+++ githubActionRun.py
@@ -1,5 +1,8 @@
 import json, asyncio, os
 from doTasks import runTasks
+from setup_config import HEADLESS
+
+HEADLESS=True  # 确保在 GitHub Actions 中以无头模式运行
 
 # 加载配置和用户数据
 user_data_json = os.getenv("USER_DATA", None)
